8bfdfb96f82bacd9db18c1169084ef1b3024d318
Adrian Marklund <adrian.marklund@teledyneflir.com>
2022-10-20 12:55:42 +0200

    kbdsecret: Print product name and serial
    * Read EEPROM for product info
    * Compute console size using display_info entry
    * Position cursor with ansi esc codes
    
    [BC-226]

--- flir/src/cmd_kbdsecret.c
+++ flir/src/cmd_kbdsecret.c
@@ -1,22 +1,118 @@
 // SPDX-License-Identifier: GPL-2.0+
 #include <command.h>
 #include <linux/delay.h>
+#include <errno.h>
+#include <vsprintf.h>
 #include <stdio_dev.h>
-#include "../../../flir/include/cmd_kbd.h"
-#include "../../../flir/include/cmd_kbdsecret.h"
+#include <asm/mach-imx/video.h>
+#include <video_font.h>
+#include "../include/cmd_kbd.h"
+#include "../include/cmd_kbdsecret.h"
+#include "../include/cmd_recoverykey.h"
+#include "../include/eeprom.h"
 
 #define ESC "\x1b"
 #define CSI "\x1b["
+#define CLR_LINE CSI "2K"
 
-void print_display(char *s)
+// TODO: Centralize EEPROM address info
+#define EEPROM_BUS_ID (2)
+#define MAIN_EEPROM_I2C_ADDR (0xae)
+#define MAIN_EEPROM_I2C_OFFS (0x40)
+
+static struct stdio_dev *sdev;
+struct stdio_dim {
+	unsigned int rows;
+	unsigned int cols;
+};
+
+static struct stdio_dim sdim;
+
+static inline void print_display(char *s)
 {
-	struct stdio_dev *dev = NULL;
+	sdev->puts(sdev, s);
+}
 
-	dev = stdio_get_by_name("vidconsole");
-	if (!dev)
-		return;
+static int init_stdio(void)
+{
+	sdev = stdio_get_by_name("vidconsole");
+	cond_log_return(!sdev, -ENODEV, "Cannot find 'vidconsole'\n");
+	return 0;
+}
 
-	dev->puts(dev, s);
+static int compute_stdio_dimensions(void)
+{
+	int i;
+	struct display_info_t const *pinfo;
+	const char *pname = env_get("panel");
+
+	cond_log_return(!pname, -ENODEV, "Missing 'panel' in environment\n");
+	for (i = 0; i < display_count; i++)
+		if (!strcmp(pname, displays[i].mode.name)) {
+			pinfo = &displays[i];
+			break;
+		}
+	cond_log_return(!pinfo, -ENODEV, "No panel '%s' is defined\n", pname);
+	cond_log_return(!pinfo->mode.xres || !pinfo->mode.yres, -EINVAL,
+			"Illegal panel dim: (%d, %d)\n", pinfo->mode.xres, pinfo->mode.yres);
+
+	sdim.rows = pinfo->mode.yres / VIDEO_FONT_HEIGHT;
+	sdim.cols = pinfo->mode.xres / VIDEO_FONT_WIDTH;
+
+	return 0;
+}
+
+static int set_cursor_pos(unsigned int row, unsigned int col)
+{
+	char buf[16];
+
+	cond_log_return(row >= sdim.rows || col >= sdim.cols, -EINVAL,
+			"Illegal cursor position (%u, %u)\n", row, col);
+	snprintf(buf, sizeof(buf), "%s%u;%uH", CSI, row, col);
+	print_display(buf);
+
+	return 0;
+}
+
+static int print_recovery_banner(void)
+{
+	unsigned int row;
+	unsigned int col;
+	struct eeprom prodinfo = {
+		.i2c_bus = EEPROM_BUS_ID,
+		.i2c_address = MAIN_EEPROM_I2C_ADDR,
+		.i2c_offset = MAIN_EEPROM_I2C_OFFS
+	};
+	char msg[32] = "Recovery Mode";
+	unsigned int mlen = strlen(msg);
+
+	set_cursor_pos(1, 1);
+	print_display(CLR_LINE);
+
+	if (eeprom_read_product(&prodinfo))
+		goto bail_out;
+
+	row = sdim.rows * 2 / 3;
+	col = (sdim.cols - mlen) / 2;
+	if (set_cursor_pos(row, col))
+		goto bail_out;
+	print_display(msg);
+
+	snprintf(msg, sizeof(msg), "Product: %s", prodinfo.product_name);
+	if (set_cursor_pos(row + 2, col))
+		goto bail_out;
+	print_display(msg);
+	snprintf(msg, sizeof(msg), " Serial: %d", prodinfo.product_serial);
+	if (set_cursor_pos(row + 3, col))
+		goto bail_out;
+	print_display(msg);
+
+	return 0;
+
+bail_out:
+	set_cursor_pos(1, 1);
+	print_display("Recovery");
+	return 0;
 }
 
 /*
@@ -26,7 +122,7 @@ void print_display(char *s)
  *        >0 when key-press detected
  *        <0 on error
  */
-int poll_key(char k)
+static int poll_key(char k)
 {
 	int numpressed;
 	int timeout = 300;
@@ -60,16 +156,22 @@ int poll_key(char k)
 
 static int do_kbd_secret(struct cmd_tbl *cmdtp, int flag, int argc, char * const argv[])
 {
+	int ret;
 	char *chp = secret;
 
 	//MSD_LOAD button overrides security check
 	if (flir_get_safe_boot())
 		return 0;
 
+	ret = init_stdio();
+	return_on_status(ret, "fb-stdio error\n");
+
+	ret = compute_stdio_dimensions();
+	return_on_status(ret, "fb-stdio error\n");
+
 	// Clear line and indicate ready-for-secret
-	print_display(CSI "2K");
-	print_display("\r");
-	print_display(":");
+	set_cursor_pos(1, 1);
+	print_display(CLR_LINE ":");
 
 	while (*chp) {
 		int result = poll_key(*chp);
@@ -90,13 +192,12 @@ static int do_kbd_secret(struct cmd_tbl *cmdtp, int flag, int argc, char * const
 	if (*chp)
 		print_display("boot");
 	else
-		print_display("recovery");
+		print_recovery_banner();
 
 	return '\0' != *chp;
 }
 
-U_BOOT_CMD(
-	kbd_secret, 1, 1, do_kbd_secret,
-	"",
-	"Returns 0 (true) to shell if success."
+U_BOOT_CMD(kbd_secret, 1, 1, do_kbd_secret,
+	   "",
+	   "Returns 0 (true) to shell if success."
 );
