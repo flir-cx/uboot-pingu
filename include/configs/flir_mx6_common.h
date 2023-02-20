8232e1821fb702397afe77f8895036fd446d8241
bo svangård <bo.svangard@flir.se>
2023-02-20 13:00:58 +0100

    U-boot split CONFIG_EXTRA_ENV_SETTINGS to commands and variables
    Split CONFIG_EXTRA_ENV_SETTINGS to CONFIG_EXTRA_ENV_COMMANDS and CONFIG_EXTRA_ENV_VARIABLES

--- include/configs/flir_mx6_common.h
+++ include/configs/flir_mx6_common.h
@@ -109,17 +109,10 @@
 #error "CONFIG_FLIR_MFG is not set to a valid value!"
 #endif /* CONFIG_FLIR_MFG */
 
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	CONFIG_DEFAULT_NETWORK_SETTINGS \
-	RANDOM_UUIDS \
-	"script=boot.scr\0" \
+#define CONFIG_EXTRA_ENV_COMMANDS \
 	"loadscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script}\0" \
 	"loadscript4=ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} " \
 		"boot/${script}\0" \
-	"uimage=uImage\0" \
-	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
-	CONFIG_FLIR_DEFAULT_DTB      \
-	"select_fdt_script=select-fdt.uscr\0" \
 	"update-fdt=" \
 			  "if ext4load mmc ${mmcdev}:${mmcpart} ${tempaddr} /boot/update-fdt.uscr; then " \
 					"source ${tempaddr}; " \
@@ -140,22 +133,8 @@
 		"else;" \
 			"setenv fdt_file ${fdt_file_default};" \
 		"fi\0" \
-	"bootlogo=bootlogo.bmp.gz\0" \
 	"loadsplash=run select_boot; ext4load mmc ${mmcdev}:${mmcpart} ${tempaddr} /boot/${bootlogo};cp.w ${tempaddr} ${splashimage} ${filesize}\0" \
-	"splashfile=/boot/bootlogo.bmp.gz\0" \
-	"splashimage=0x17000002\0" \
-	"splashsource=mmc_fs\0" \
-	"tempaddr=0x16000000\0" \
-	"fdt_addr=0x18000000\0" \
-	"initrd_addr=0x19000000\0" \
-	"initrd_file=uRamdisk.img\0" \
-	"boot_fdt=try\0" \
-	"ip_dyn=yes\0" \
-	"console=" CONSOLE_DEV "\0" \
-	"fdt_high=0xffffffff\0" \
-	"initrd_high=0xffffffff\0" \
 	"hw_start=checkCharger; loadFPGA t\0" \
-	"mmcpart=1\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} ${smp} " \
 		"${rootfs} rootwait rw ethaddr=${ethaddr} " \
 		"wlanaddr=${wlanaddr} btaddr=${btaddr} ${bootargs_once} " \
@@ -189,7 +168,6 @@
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${rootpath},v3,tcp " \
 		"${extra_bootargs} ${charge_state}\0" \
-	"extra_bootargs=quiet\0" \
 	"netboot=echo Booting from net ...; " \
 		"run hw_start; " \
 		"run netargs; " \
@@ -218,14 +196,6 @@
 		"run recargs; " \
 		"run hw_start;" \
 		"bootm ${loadaddr} ${initrd_addr} ${fdt_addr}; \0" \
-	"parts_flir=\"uuid_disk=${uuid_disk};" \
-		"start=2MiB,name=recovery,size=40MiB,uuid=${part1_uuid};" \
-		"name=rootfs1,size=504MiB,uuid=${part2_uuid};" \
-		"name=rootfs2,size=504MiB,uuid=${part3_uuid};" \
-		"name=rootfsrw,size=128MiB,uuid=${part4_uuid};" \
-		"name=apps,size=512MiB,uuid=${part5_uuid};" \
-		"name=data,size=512MiB,uuid=${part6_uuid};" \
-		"name=spare,size=-,uuid=${part7_uuid};\" \0" \
 	"partition_mmc_flir=mmc rescan; " \
 		"if mmc dev ${mmcdev} 0; then " \
 			"gpt write mmc ${mmcdev} ${parts_flir}; " \
@@ -237,7 +207,6 @@
 			"else; " \
 			"fi; " \
 		"fi; \0" \
-	"system_active=system1\0" \
 	"setup_boot=bootargs_once=init=/sbin/preinit; " \
 		"run select_boot;\0" \
 	"select_boot=" \
@@ -270,7 +239,6 @@
 		"setenv mmcpart 1; " \
 		"run loadfdt; run loadinitrd; run loaduimage; " \
 		"bootm ${loadaddr} ${initrd_addr} ${fdt_addr};\0" \
-	"mmcroot=PARTUUID=1c606ef5-f1ac-43b9-9bb5-d5c578580b6b\0" \
 	"bootargs_mmc_linux=setenv bootargs console=${console},${baudrate} " \
 		"${bootargs_linux} root=${mmcroot} rootwait rw " \
 		"${bootargs_once} ${extra_bootargs}\0" \
@@ -279,14 +247,6 @@
 		"ip=dhcp nfsroot=${serverip}:${rootpath},v3,tcp " \
 		"${bootargs_once} ${extra_bootargs}\0" \
 	"bootargs_nfs_linux=run bootargs_tftp_linux\0" \
-	"parts_linux=\"uuid_disk=${uuid_disk};" \
-		"start=2MiB," \
-		"name=linux,size=64MiB,uuid=${part1_uuid};" \
-		"name=linux2,size=64MiB,uuid=${part2_uuid};" \
-		"name=rootfs,size=1GiB,uuid=${part3_uuid};" \
-		"name=rootfs2,size=1GiB,uuid=${part4_uuid};" \
-		"name=userfs,size=-,uuid=${part5_uuid};" \
-		"\" \0" \
 	"partition_mmc_linux=mmc rescan;" \
 		"if mmc dev ${mmcdev} 0; then " \
 			"gpt write mmc ${mmcdev} ${parts_linux};" \
@@ -298,7 +258,55 @@
 			"else;" \
 			"fi;" \
 		"fi;\0" \
+	"" /* EOL */
+
+#define CONFIG_EXTRA_ENV_VARIABLES \
+	"script=boot.scr\0" \
+	"uimage=uImage\0" \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	CONFIG_FLIR_DEFAULT_DTB      \
+	"select_fdt_script=select-fdt.uscr\0" \
+	"bootlogo=bootlogo.bmp.gz\0" \
+	"splashfile=/boot/bootlogo.bmp.gz\0" \
+	"splashimage=0x17000002\0" \
+	"splashsource=mmc_fs\0" \
+	"tempaddr=0x16000000\0" \
+	"fdt_addr=0x18000000\0" \
+	"initrd_addr=0x19000000\0" \
+	"initrd_file=uRamdisk.img\0" \
+	"boot_fdt=try\0" \
+	"ip_dyn=yes\0" \
+	"console=" CONSOLE_DEV "\0" \
+	"fdt_high=0xffffffff\0" \
+	"initrd_high=0xffffffff\0" \
+	"mmcpart=1\0" \
+	"extra_bootargs=quiet\0" \
+	"system_active=system1\0" \
+	"mmcroot=PARTUUID=1c606ef5-f1ac-43b9-9bb5-d5c578580b6b\0" \
+	"parts_flir=\"uuid_disk=${uuid_disk};" \
+		"start=2MiB,name=recovery,size=40MiB,uuid=${part1_uuid};" \
+		"name=rootfs1,size=504MiB,uuid=${part2_uuid};" \
+		"name=rootfs2,size=504MiB,uuid=${part3_uuid};" \
+		"name=rootfsrw,size=128MiB,uuid=${part4_uuid};" \
+		"name=apps,size=512MiB,uuid=${part5_uuid};" \
+		"name=data,size=512MiB,uuid=${part6_uuid};" \
+		"name=spare,size=-,uuid=${part7_uuid};\" \0" \
+	"parts_linux=\"uuid_disk=${uuid_disk};" \
+		"start=2MiB," \
+		"name=linux,size=64MiB,uuid=${part1_uuid};" \
+		"name=linux2,size=64MiB,uuid=${part2_uuid};" \
+		"name=rootfs,size=1GiB,uuid=${part3_uuid};" \
+		"name=rootfs2,size=1GiB,uuid=${part4_uuid};" \
+		"name=userfs,size=-,uuid=${part5_uuid};" \
+		"\" \0" \
 	"mmcdev=0\0" \
+	"" /* EOL */
+
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	CONFIG_DEFAULT_NETWORK_SETTINGS \
+	CONFIG_EXTRA_ENV_COMMANDS \
+	CONFIG_EXTRA_ENV_VARIABLES \
+	RANDOM_UUIDS \
 	""	/* end line */
 
 #define CONFIG_ARP_TIMEOUT 200UL
