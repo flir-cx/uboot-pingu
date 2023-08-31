9cbcb1652e52fb04ab3dd64c81a5904ef56fb381
Jonas Rydow <jonas.rydow@teledyneflir.com>
2023-08-31 08:59:14 +0200

    BC-470 Fix selection of display and touch for both 3.14 and 5.10 kernels

--- board/flir/mx6ec101/mx6ec101.c
+++ board/flir/mx6ec101/mx6ec101.c
@@ -1625,17 +1625,20 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 	if (detect_truly(NULL)) {
 		/* Point to truly display instead of orise */
 		do_fixup_by_path(blob, "/fb@0", "mode_str", "TRULY-VGA", 10, 0);
-		do_fixup_by_path(blob, "/soc/bus@2100000/mipi@21e0000", "lcd_panel",
-				 "TRULY-VGA", 10, 0);
+		do_fixup_by_compat(blob, "fsl,imx6dl-mipi-dsi", "lcd_panel",
+				   "TRULY-VGA", 10, 0);
 
 		/* Set the status of the touch of the truly display to ok, */
 		/* it should be disabled by default from the device tree. */
-		do_fixup_by_path(blob, "/soc/bus@2100000/i2c@21f8000/tsc@70", "status",
-				 "okay", 5, 0);
+		do_fixup_by_compat(blob, "cypress,tt21000", "status",
+				   "okay", 5, 0);
 		/* Set the status of the orise touch to disabled, it should */
-		/* be enabled by default from th edevice tree */
-		do_fixup_by_path(blob, "/soc/bus@2100000/i2c@21f8000/edt_ft5336@38", "status",
-				 "disabled", 9, 0);
+		/* be enabled by default from the edevice tree */
+		/* The compatible strings differ from 3.15 (edt,ft5x06) in 5.10 (edt,ft5336)*/
+		do_fixup_by_compat(blob, "edt,ft5336", "status",
+				   "disabled", 9, 0);
+		do_fixup_by_compat(blob, "edt,ft5x06", "status",
+				   "disabled", 9, 0);
 	}
 	if (IS_ENABLED(CONFIG_VIDEO_IPUV3)) {
 		int temp[2];
