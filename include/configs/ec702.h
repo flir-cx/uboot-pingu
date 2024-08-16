455490c413841347713df392da74e43ecca96075
Adrian Marklund <adrian.marklund@teledyneflir.com>
2024-08-16 09:58:01 +0200

    ec702: Config correct EEPROM bus
    Mainboard i2c EEPROM is on bus 2.
    
    [LEIA-863]

--- include/configs/ec702.h
+++ include/configs/ec702.h
@@ -26,7 +26,7 @@
 #define CONFIG_IMX6_LDO_BYPASS
 
 // Config main EEPROM here, unless the entire EEPROM driver is used
-#define CONFIG_SYS_I2C_EEPROM_BUS      0
+#define CONFIG_SYS_I2C_EEPROM_BUS      2
 #define CONFIG_SYS_I2C_EEPROM_ADDR     0xae
 #define CONFIG_SYS_I2C_EEPROM_ADDR_LEN 1
 
